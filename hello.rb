b3729ac329880cf8626f5594cb6a3069cf6a04c8
James B Kemp <james@bibble.com>
2014-05-26 16:58:11 +0100

    changed comment to 2br rebase

--- hello.rb
+++ hello.rb
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-# I'm editing this from branch '2br' via nano.
-=======
-# this is a change made in branch 3br.
-#!/usr/bin/env ruby
-
->>>>>>> 3br
-=======
-# this was an old commit, moved into 4br, with this new comment added
->>>>>>> 4br
+# new comment on 2br during rebase testing.
 # Default is "world"
 
 name = ARGV.first || "World"
